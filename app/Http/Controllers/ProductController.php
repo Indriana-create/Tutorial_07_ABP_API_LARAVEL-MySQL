162990ac24af5d1b6abfac3f1df7bae10e87ffcd
Indriana Noviyanti <indriana@student.telkomuniversity.ac.id>
2024-06-03 09:25:53 +0700

    Add Validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,66 +3,79 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Product; 
+use App\Models\Product;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
 
 class ProductController extends Controller
 {
+    use ValidatesRequests;
+
     public function index()
-    { 
+    {
         $prods = Product::get();
         return view('product.index', ['list' => $prods]);
     }
-    
-    public function create() 
-    { 
-        return view('product.form', [ 
-            'title' => 'Tambah', 
-            'method' => 'POST', 
-            'action' => 'product' 
-        ]); 
-    }
-    
-    public function store(Request $request) 
-    { 
-        $prod = new Product; 
-        $prod->name = $request->name; 
-        $prod->price = $request->price; 
-        $prod->save(); 
-        return redirect('/product')->with('msg', 'Tambah berhasil');
-    } 
-    
-    public function show($id) 
+
+    public function create()
     {
-        return Product::find($id); 
-    } 
-    
-    public function edit($id) 
-    { 
-        return view('product.form', [ 
-            'title' => 'Edit', 
-            'method' => 'PUT', 
-            'action' => "product/$id", 
-            'data' => Product::find($id) 
-        ]); 
+        return view('product.form', [
+            'title' => 'Tambah',
+            'method' => 'POST',
+            'action' => 'product'
+        ]);
     }
 
-    public function update(Request $request, $id) 
-    { 
-        $prod = Product::find($id); 
-        $prod->name = $request->name; 
-        $prod->price = $request->price; 
-        $prod->save(); 
-        return redirect('/product')->with('msg', 'Edit berhasil'); 
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:4',
+            'price' => 'required|integer|min:1000000'
+        ]);
+
+            $prod = new Product;
+            $prod->name = $request->name;
+            $prod->price = $request->price;
+            $prod->save();
+            return redirect('/product')->with('msg', 'Tambah berhasil');
     }
 
-    public function destroy($id) 
-    { 
+    public function show($id)
+    {
+        return Product::find($id);
+    }
+
+    public function edit($id)
+    {
+        return view('product.form', [
+            'title' => 'Edit',
+            'method' => 'PUT',
+            'action' => "product/$id",
+            'data' => Product::find($id)
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:4',
+            'price' => 'required|integer|min:1000000'
+        ]);
+            $prod = Product::find($id);
+            $prod->name = $request->name;
+            $prod->price = $request->price;
+            $prod->save();
+            return redirect('/product')->with('msg', 'Edit berhasil');
+    }
+
+    public function destroy($id)
+    {
         Product::destroy($id);
-        // atau 
+        // atau
 
-    /* $prod = Product::find($id); 
+    /* $prod = Product::find($id);
 
-    $prod->delete(); */ 
-        return redirect('/product')->with('msg', 'Hapus berhasil'); 
+    $prod->delete(); */
+        return redirect('/product')->with('msg', 'Hapus berhasil');
     }
 }
